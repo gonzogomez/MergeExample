42a316a113305cd63adc54ed216820088323b3ae
Gonzalo Gomez <gonzalogomez@wl-dhcp198-245.mines.edu>
2013-10-25 09:18:06 -0600

    Dog and Cat classes created

---
+++ Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+	private int age;
+	private int size;
+
+	public Cat(int age, int size) {
+		this.age = age;
+		this.size = size;
+	}
+
+	public void meow(){
+		
+	}
+}
---
+++ Dog.java
@@ -0,0 +1,13 @@
+public class Dog {
+	private int age;
+	private int size;
+
+	public Dog(int age, int size) {
+		this.age = age;
+		this.size = size;
+	}
+
+	public void bark(){
+		
+	}
+}
